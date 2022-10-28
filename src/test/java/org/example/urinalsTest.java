f6496267d853fcc0c756726c3553196909bc9664
apathuri99 <apathuri@asu.edu>
2022-10-27 23:33:27 -0700

    Final Commit

--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -10,19 +10,36 @@ class urinalsTest {
     void main() {
 
     }
-
     @Test
-    void getString() {
-        assertEquals(true,urinals.getString());
-    }
+    void goodString() {
+        System.out.println("=======Akhila Pathuri========TEST ONE EXECUTED");
+        assertFalse(urinals.goodString("10011001"));
+        assertTrue(urinals.goodString("0101001001"));
+        assertTrue(urinals.goodString("10001"));
+        assertEquals(-1,urinals.countUrinals("10011001"));
+        assertEquals(0,urinals.countUrinals("0101001001"));
+        assertEquals(1,urinals.countUrinals("10001"));
+        System.out.println(urinals.goodString("10011001"));
+        System.out.println(urinals.countUrinals("10011001"));
+        System.out.println(urinals.goodString("0101001001"));
+        System.out.println(urinals.countUrinals("0101001001"));
+        System.out.println(urinals.goodString("10001"));
+        System.out.println(urinals.countUrinals("10001"));
 
-    @Test
-    void openFile() {
-        assertEquals("Should be implemented",urinals.openFile());
-    }
 
-    @Test
-    void countUrinals() {
-        assertEquals(1,urinals.countUrinals());
     }
+//    @Test
+//    void getString() {
+//        assertEquals(true,urinals.getString());
+//    }
+//
+//    @Test
+//    void openFile() {
+//        assertEquals("Should be implemented",urinals.openFile());
+//    }
+
+//    @Test
+//    void countUrinals() {
+//        assertEquals(1,urinals.countUrinals());
+//    }
 }
